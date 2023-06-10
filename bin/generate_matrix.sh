b357a2ada6ec6388ab447901819cac157d5c7166
saumyaborwankar <17bec095@nirmauni.ac.in>
2023-06-10 11:32:40 -0500

    made changes in generate script

--- bin/generate_matrix.sh
+++ bin/generate_matrix.sh
@@ -5,13 +5,32 @@ set -e
 # gives the path of the dockerfiles that have been modified/added in the last commit to main
 changed_dockerfile_path=$(git diff --name-only HEAD^ HEAD | grep '/Dockerfile$')
 
+# Count the number of file paths
+total_files=$(echo "${changed_dockerfile_path}" | wc -l)
+
 # i dont exactly know why we write include in the json that we are creating for the matrix but if it works it works
 # if someone knows let me know. Thanks
 echo -n '{"include": ['
 
-# loop over the filepaths
+# loop over the filepaths to make the context and tag for github action
+index=0
 for file_path in ${changed_dockerfile_path}; do
-    echo -n "{\"path\": \"${file_path}\"}"
+    
+    docker_tag=$(basename $(dirname "$file_path"))
+    docker_name=$(basename $(dirname $(dirname "$file_path")))
+    university=$(basename $(dirname $(dirname $(dirname "$file_path"))))
+
+    # removing the last Dockerfile from the file path
+    updated_path=$(echo "$file_path" | sed 's/\/Dockerfile$//')
+
+    echo -n "{\"context\": \"${updated_path}\", \"tags\": \"${university}/${docker_name}:${docker_tag}\"},"
+
+
+
+    # ((index++))
+    # if [ "${index}" -ne "${total_files}" ]; then
+    #         echo -n "last runnnnnnnnnn"     # Add space if it's not the last run
+    # fi
 done
 
 echo -n "]}"
