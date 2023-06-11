7e154f295a442c6b80dbcbf659af262a8c45f65a
saumyaborwankar <thesaumyaborwankar@gmail.com>
2023-06-10 20:49:30 -0500

    p

--- bin/generate_matrix.sh
+++ bin/generate_matrix.sh
@@ -23,8 +23,8 @@ for file_path in ${changed_dockerfile_path}; do
     # removing the last Dockerfile from the file path
     updated_path=$(echo "$file_path" | sed 's/\/Dockerfile$//')
 
-    echo -n '{\"context\": \""'"${updated_path}"'"\", \"tags\": \""'"${docker_name}"'":"'"${docker_tag}"'"\"},'
-    # echo -n "{\"version\": \"${docker_name}\", \"adapter\": \"${docker_name}\"}"
+    echo -n '{\"context\": \""'"${updated_path}"'"\", \"tags\": \""'"${docker_name}"'":"'"${docker_tag}"'"\"},' #for one name
+    # echo -n '{\"context\": \""'"${updated_path}"'"\", \"tags\": \""'"${university}"'"/"'"${docker_name}"'":"'"${docker_tag}"'"\"},' # for 2 names
 
 
     # ((index++))
